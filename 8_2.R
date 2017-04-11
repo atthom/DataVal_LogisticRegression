0d19e2495a336c4b5b88fd43a0715ae9c5611d67
Robin ALONZO <alonzo.robin7@gmail.com>
2017-04-11 16:53:01 +0200

    Question 5 qui a l'air de fonctionner

--- 8_2.R
+++ 8_2.R
@@ -1,4 +1,4 @@
-n = 10
+n = 1000
 m0 = 0.5
 m1 = 1.1
 sd = 1.2
@@ -33,52 +33,26 @@ xi = t(cbind(rep(1,length(x)),x))
 #print(xi)
 yi = c(y1,y2)
 #print(yi)
-bc0 = 1
-bc1 = 1
+bc0 = 0.3
+bc1 = 1.7
 bc = c(bc0,bc1)
 
-gamma = 0.01
+gamma = 0.0001
 
-M = 1
+M = 1000
 for(t in 1:M){
-    #We compute the gradient
-    #print("bc")  
-    #print(bc)
-    #print("xi")  
-    #print(xi)
-    #print(bc[1]+bc[2]*x)
-    pc = logistic(bc[1]+bc[2]*x)
-    #cat("temp = ",temp,"\n")
-    #print("yi")
-    #print(yi)
-    #print("pc")
-    #print(pc)
-    #print("temp")
-    #print(temp)
-    #print("xi*temp")
-    #print(xi*temp)
-    #print("rowSums(xi*temp)")
-    #print(rowSums(xi*temp))
-    #print(dim(xi))
-    #print(dim(temp))
-    gradJ = rowSums(xi*(yi-pc))
-    cat("gradJ = ",gradJ,"\n")
-    
-    #gradJb0 = sum(x[0:t]*(y-pc0))
-    #gradJb1 = sum(x[0:t]*(y-pc1))
-    #gradJb0 = sum(y*x)-sum((exp(bc0)/(1+exp(bc0)))*x)
-    #gradJb1 = sum(y*x)-sum((exp(bc1*x)/(1+exp(bc1*x)))*x)
-    #cat("gradJb0 = ",gradJb0,"\n")
-    #cat("gradJb1 = ",gradJb1,"\n")
+  print(bc)
+  #We compute the gradient
+  pc = logistic(bc[1]+bc[2]*x)
+  gradJ = rowSums(xi%*%(yi-pc))
+  #print(gradJ)
+
+  #We update the estimates 
+  bc = bc + gamma*gradJ
   
-    #We update the estimates 
-    bc = bc + gamma*gradJ
-    #bc0 = bc0 + gamma*gradJb0
-    #bc1 = bc1 + gamma*gradJb1
-    
-    #We compute the maximum likelihood function
-    J = sum(yi*bc*xi)-sum(log(1+exp(bc*xi)))
-    cat("J = ",J,"\n")
+  #We compute the maximum likelihood function
+  J = sum(yi*(bc[1]+bc[2]*x))-sum(log(1+exp(bc[1]+bc[2]*x)))
+  cat("J = ",J,"\n")
 }
 
 print(bc)
