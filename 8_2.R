b2787a9e08b1cde04505ef193b8c9766b318c2e5
Robin ALONZO <alonzo.robin7@gmail.com>
2017-04-14 17:17:33 +0200

    Cleaning and comments

--- 8_2.R
+++ 8_2.R
@@ -32,59 +32,32 @@ generateBinaryResponse<-function(x,b0,b1){
   return(y)
 }
 
-j_funct <- function(xi, yi, m, bc) {
-  all_sum =  c(0,0)
-  for(i in 1:m) {
-    one = (get_pc(bc, xi[i]) - yi[i])* xi[i]
-    all_sum = c(all_sum, one)
-  }
-  return((1/m)*sum(all_sum))
-}
-
-gradient<-function(xi,yi,bc){
-  
-  correction = yi - get_pc(bc, xi)
-  print( get_pc(bc, xi))
-  
-  gradJ = rowSums(xi*correction)
-  
-  return(gradJ)
-}
-
-
 y1 = generateBinaryResponse(x1,b0,b1)
 
 #4.
 y2 = generateBinaryResponse(x2,b0,b1)
 
 #5.
-J<-function(x,y,b0,b1){
-  return(sum(y%*%(b0+b1*x))-sum(log(1+exp(b0+b1*x))))
-}
-
 x = c(x1,x2)
 xi = t(cbind(rep(1,length(x)),x))
 yi = c(y1,y2)
 
-bc0 = 0.3
-bc1 = 1.7
+#We initialize the estimated betas
+bc0 = 1
+bc1 = 1
 bc = c(bc0,bc1)
 
-gamma = 0.0001
-
-M = 200
-ccc = c(0,0)
-
-compute_j<-function(yi, beta0, beta1,x) {
-  # function that compute J of beta
+#Maximum likelihood function
+compute_J<-function(yi, beta0, beta1,x) {
   return(sum(yi%*%(beta0+beta1*x))-sum(log(1+exp(beta0+beta1*x))))
 }
 
-bc_0_all = bc[1]
-bc_1_all = bc[2]
-
+ccc = c(0,0)
 all_gradJ = c()
 
+#Gradient ascent loop
+gamma = 0.0001
+M = 200
 for(t in 1:M){
   #We compute the gradient
   pc = logistic(bc[1]+bc[2]*x)
@@ -97,33 +70,32 @@ for(t in 1:M){
   bc = bc + gamma*gradJ
   
   #We compute the maximum likelihood function
-  J = compute_j(yi, bc[1], bc[2], x)
+  J = compute_J(yi, bc[1], bc[2], x)
   cat("J = ",J,"\n")
   ccc = c(ccc, J)
-  
-  
 }
+cat("estimated beta = ",bc,"\n")
 
+#We plot the norm of the gradient of the maximum likelihood function
 plot(all_gradJ, title(main = "Norme du gradient en fonction de t", xlab = "iterations", ylab = "Norme du gradient"))
 
 
+#We plot a heatmap of the maximum likelihood function
 bc_0_all = seq(0, 2, 0.1)
 bc_1_all = seq(0, 2, 0.1)
 ll = length(bc_1_all)
 mat = matrix(rep(0, (ll)*(ll)), ncol=ll, nrow=ll)
-
 for(i in 1:(length(bc_1_all))) {
   for(j in 1:(length(bc_1_all))) {
     mat[i,j] =  compute_j(yi, bc_0_all[i], bc_1_all[j], x)
   }
 }
-
 colnames(mat) = paste("Beta 0 : ", bc_0_all , sep = "")
 rownames(mat) = paste("Beta 1 : ", bc_1_all, sep = "")
 pheatmap(mat, cluster_row = FALSE, cluster_col = FALSE, color= heat.colors(254, alpha = 1),
          main = "Heatmap de la fonction de coup (J) en fonction de beta 0 et de beta 1")
 
-cat("estimated beta = ",bc,"\n")
+
 
 #7.
 Prm0 = sum(generateBinaryResponse(x1,bc[1],bc[2]))/n
